bc3c8400be767b940656624a0b63a903641eaf68
tedx-uofw <tedxuofw@uw.edu>
2024-05-13 22:18:13 -0700

    adding past events

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Footer from './components/footer/footer';
 import Landing from "./components/landing"
 import About from "./components/about/about"
 import Contact from "./components/contact/contact"
-// import PastEvents from "./components/past-events/PastEvents"
+import PastEvents from "./components/past-events/PastEvents"
 import FAQ from "./components/faq/faq"
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -25,7 +25,7 @@ function App() {
               <Route path="/" element ={ <Landing /> } />
               <Route path="/about" element = { <About /> } />
               <Route path="/contact" element = { <Contact /> } />
-              {/* <Route path="/past-events" element = { <PastEvents /> } /> */}
+              <Route path="/past-events" element = { <PastEvents /> } />
               <Route path="/faq" element = { <FAQ /> } />
               <Route path="*" element={<div> 404 Not Found </div>}></Route>
             </Routes>
