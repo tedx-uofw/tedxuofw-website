a9abbdb3335c666c377a38d9c4a8fe73cabb8e19
mmel4nie <87841303+mmel4nie@users.noreply.github.com>
2024-05-15 16:57:43 -0700

    fix faq css

--- src/components/faq/faq.jsx
+++ src/components/faq/faq.jsx
@@ -23,7 +23,7 @@ const Faq = () => {
         <h2>General FAQ</h2>
         <div className="dropdown-join-container">
           <div className="dropdown-ctr">
-            <button className={`accordion ${activeIndex === 0 ? 'active' : ''}`} onClick={() => togglePanel(0)}>I’d like to know about upcoming TEDxUofW events?<img src={downArrow} alt="down arrow img" className={`arrow ${activeIndex === 0 ? 'rotate' : ''}`} /></button>
+            <button className={`accordion ${activeIndex === 0 ? 'active' : ''}`} onClick={() => togglePanel(0)}>I’d like to know about upcoming TEDxUofW events?<div className="arrowContainer"><img src={downArrow} alt="down arrow img" className={`arrow ${activeIndex === 0 ? 'rotate' : ''}`} /></div></button>
             <div className="panel" style={{ display: activeIndex === 0 ? 'block' : 'none' }}>
               <div className="panel-text">
                 <p>You can follow our <a href="https://www.instagram.com/tedxuofw/?hl=en">Instagram</a> for regular updates and check out our upcoming
@@ -32,7 +32,7 @@ const Faq = () => {
               </div>
             </div>
 
-            <button className={`accordion ${activeIndex === 1 ? 'active' : ''}`} onClick={() => togglePanel(1)}>How can I volunteer?<img src={downArrow} alt="down arrow img" className={`arrow ${activeIndex === 1 ? 'rotate' : ''}`} /></button>
+            <button className={`accordion ${activeIndex === 1 ? 'active' : ''}`} onClick={() => togglePanel(1)}>How can I volunteer?<div className="arrowContainer"><img src={downArrow} alt="down arrow img" className={`arrow ${activeIndex === 1 ? 'rotate' : ''}`} /></div></button>
             <div className="panel" style={{ display: activeIndex === 1 ? 'block' : 'none' }}>
               <div className="panel-text">
                 <p>Thank you for your interest in helping out! TEDxUofW wouldn’t exist without the
@@ -41,7 +41,7 @@ const Faq = () => {
               </div>
             </div>
 
-            <button className={`accordion ${activeIndex === 2 ? 'active' : ''}`} onClick={() => togglePanel(2)}>Are TEDxUofW events open to the public?<img src={downArrow} alt="down arrow img" className={`arrow ${activeIndex === 2 ? 'rotate' : ''}`} /></button>
+            <button className={`accordion ${activeIndex === 2 ? 'active' : ''}`} onClick={() => togglePanel(2)}>Are TEDxUofW events open to the public?<div className="arrowContainer"><img src={downArrow} alt="down arrow img" className={`arrow ${activeIndex === 2 ? 'rotate' : ''}`} /></div></button>
             <div className="panel" style={{ display: activeIndex === 2 ? 'block' : 'none' }}>
               <div className="panel-text">
                 <p>Yes, our events are open to everyone! However, tickets must be purchased, which
@@ -50,7 +50,7 @@ const Faq = () => {
               </div>
             </div>
 
-            <button className={`accordion ${activeIndex === 3 ? 'active' : ''}`} onClick={() => togglePanel(3)}>Can I watch past TEDxUofW talks online?<img src={downArrow} alt="down arrow img" className={`arrow ${activeIndex === 3 ? 'rotate' : ''}`} /></button>
+            <button className={`accordion ${activeIndex === 3 ? 'active' : ''}`} onClick={() => togglePanel(3)}>Can I watch past TEDxUofW talks online?<div className="arrowContainer"><img src={downArrow} alt="down arrow img" className={`arrow ${activeIndex === 3 ? 'rotate' : ''}`} /></div></button>
             <div className="panel" style={{ display: activeIndex === 3 ? 'block' : 'none' }}>
               <div className="panel-text">
                 <p>Yes! We upload videos of our talks to the official TEDx YouTube channel after each
@@ -59,7 +59,7 @@ const Faq = () => {
               </div>
             </div>
 
-            <button className={`accordion ${activeIndex === 4 ? 'active' : ''}`} onClick={() => togglePanel(4)}>Still have questions?<img src={downArrow} alt="down arrow img" className={`arrow ${activeIndex === 4 ? 'rotate' : ''}`} /></button>
+            <button className={`accordion ${activeIndex === 4 ? 'active' : ''}`} onClick={() => togglePanel(4)}>Still have questions?<div className="arrowContainer"><img src={downArrow} alt="down arrow img" className={`arrow ${activeIndex === 4 ? 'rotate' : ''}`} /></div></button>
             <div className="panel" style={{ display: activeIndex === 4 ? 'block' : 'none' }}>
               <div className="panel-text">
                 <p>Please check out our <a href="#speakerFaq">Speaker FAQ</a> and <a href="#sponsorFaq">Sponsor FAQ</a> for more information about
@@ -91,7 +91,7 @@ const Faq = () => {
         <h2>Speaker FAQ</h2>
         <div className="dropdown-join-container">
           <div className="dropdown-ctr">
-            <button className={`accordion ${activeIndex === 5 ? 'active' : ''}`} onClick={() => togglePanel(5)}>How do I become a speaker?<img src={downArrow} alt="down arrow img" className={`arrow ${activeIndex === 5 ? 'rotate' : ''}`} /></button>
+            <button className={`accordion ${activeIndex === 5 ? 'active' : ''}`} onClick={() => togglePanel(5)}>How do I become a speaker?<div className="arrowContainer"><img src={downArrow} alt="down arrow img" className={`arrow ${activeIndex === 5 ? 'rotate' : ''}`} /></div></button>
             <div className="panel" style={{ display: activeIndex === 5 ? 'block' : 'none' }}>
               <div className="panel-text">
                 <p>Applications will open in November for the TEDxUofW conference in the following
@@ -100,7 +100,7 @@ const Faq = () => {
               </div>
             </div>
 
-            <button className={`accordion ${activeIndex === 6 ? 'active' : ''}`} onClick={() => togglePanel(6)}>What does being a speaker entail?<img src={downArrow} alt="down arrow img" className={`arrow ${activeIndex === 6 ? 'rotate' : ''}`} /></button>
+            <button className={`accordion ${activeIndex === 6 ? 'active' : ''}`} onClick={() => togglePanel(6)}>What does being a speaker entail?<div className="arrowContainer"><img src={downArrow} alt="down arrow img" className={`arrow ${activeIndex === 6 ? 'rotate' : ''}`} /></div></button>
             <div className="panel" style={{ display: activeIndex === 6 ? 'block' : 'none' }}>
               <div className="panel-text">
                 <p>Being a speaker involves 6-10 talk development sessions over 3 months leading up
@@ -110,7 +110,7 @@ const Faq = () => {
               </div>
             </div>
 
-            <button className={`accordion ${activeIndex === 7 ? 'active' : ''}`} onClick={() => togglePanel(7)}>What are talk development sessions?<img src={downArrow} alt="down arrow img" className={`arrow ${activeIndex === 7 ? 'rotate' : ''}`} /></button>
+            <button className={`accordion ${activeIndex === 7 ? 'active' : ''}`} onClick={() => togglePanel(7)}>What are talk development sessions?<div className="arrowContainer"><img src={downArrow} alt="down arrow img" className={`arrow ${activeIndex === 7 ? 'rotate' : ''}`} /></div></button>
             <div className="panel" style={{ display: activeIndex === 7 ? 'block' : 'none' }}>
               <div className="panel-text">
                 <p>Talk development sessions are rehearsals where you'll receive feedback on your
@@ -119,14 +119,14 @@ const Faq = () => {
               </div>
             </div>
 
-            <button className={`accordion ${activeIndex === 8 ? 'active' : ''}`} onClick={() => togglePanel(8)}>How many speakers will be at the conference?<img src={downArrow} alt="down arrow img" className={`arrow ${activeIndex === 8 ? 'rotate' : ''}`} /></button>
+            <button className={`accordion ${activeIndex === 8 ? 'active' : ''}`} onClick={() => togglePanel(8)}>How many speakers will be at the conference?<div className="arrowContainer"><img src={downArrow} alt="down arrow img" className={`arrow ${activeIndex === 8 ? 'rotate' : ''}`} /></div></button>
             <div className="panel" style={{ display: activeIndex === 8 ? 'block' : 'none' }}>
               <div className="panel-text">
                 <p>There will be 5-8 speakers at the conference.</p>
               </div>
             </div>
 
-            <button className={`accordion ${activeIndex === 9 ? 'active' : ''}`} onClick={() => togglePanel(9)}>Does my talk need to be memorized?<img src={downArrow} alt="down arrow img" className={`arrow ${activeIndex === 9 ? 'rotate' : ''}`} /></button>
+            <button className={`accordion ${activeIndex === 9 ? 'active' : ''}`} onClick={() => togglePanel(9)}>Does my talk need to be memorized?<div className="arrowContainer"><img src={downArrow} alt="down arrow img" className={`arrow ${activeIndex === 9 ? 'rotate' : ''}`} /></div></button>
             <div className="panel" style={{ display: activeIndex === 9 ? 'block' : 'none' }}>
               <div className="panel-text">
                 <p>Yes! With no teleprompters available, it's crucial to have your talk memorized well in
@@ -134,7 +134,7 @@ const Faq = () => {
               </div>
             </div>
 
-            <button className={`accordion ${activeIndex === 10 ? 'active' : ''}`} onClick={() => togglePanel(10)}>What does the theme of the event have to do with me?<img src={downArrow} alt="down arrow img" className={`arrow ${activeIndex === 10 ? 'rotate' : ''}`} /></button>
+            <button className={`accordion ${activeIndex === 10 ? 'active' : ''}`} onClick={() => togglePanel(10)}>What does the theme of the event have to do with me?<div className="arrowContainer"><img src={downArrow} alt="down arrow img" className={`arrow ${activeIndex === 10 ? 'rotate' : ''}`} /></div></button>
             <div className="panel" style={{ display: activeIndex === 10 ? 'block' : 'none' }}>
               <div className="panel-text">
                 <p>While your talk doesn't need to directly align with the theme, we encourage
@@ -143,7 +143,7 @@ const Faq = () => {
               </div>
             </div>
 
-            <button className={`accordion ${activeIndex === 11 ? 'active' : ''}`} onClick={() => togglePanel(11)}>How strict are speaker deadlines?<img src={downArrow} alt="down arrow img" className={`arrow ${activeIndex === 11 ? 'rotate' : ''}`} /></button>
+            <button className={`accordion ${activeIndex === 11 ? 'active' : ''}`} onClick={() => togglePanel(11)}>How strict are speaker deadlines?<div className="arrowContainer"><img src={downArrow} alt="down arrow img" className={`arrow ${activeIndex === 11 ? 'rotate' : ''}`} /></div></button>
             <div className="panel" style={{ display: activeIndex === 11 ? 'block' : 'none' }}>
               <div className="panel-text">
                 <p>We adhere strictly to our deadlines to ensure ample time for reviewing
@@ -152,14 +152,14 @@ const Faq = () => {
               </div>
             </div>
 
-            <button className={`accordion ${activeIndex === 12 ? 'active' : ''}`} onClick={() => togglePanel(12)}>What if I need special accommodations?<img src={downArrow} alt="down arrow img" className={`arrow ${activeIndex === 12 ? 'rotate' : ''}`} /></button>
+            <button className={`accordion ${activeIndex === 12 ? 'active' : ''}`} onClick={() => togglePanel(12)}>What if I need special accommodations?<div className="arrowContainer"><img src={downArrow} alt="down arrow img" className={`arrow ${activeIndex === 12 ? 'rotate' : ''}`} /></div></button>
             <div className="panel" style={{ display: activeIndex === 12 ? 'block' : 'none' }}>
               <div className="panel-text">
                 <p>We accommodate special needs such as learning disabilities or food allergies.</p>
               </div>
             </div>
 
-            <button className={`accordion ${activeIndex === 13 ? 'active' : ''}`} onClick={() => togglePanel(13)}>Are travel costs and lodging covered by TEDxUofW?<img src={downArrow} alt="down arrow img" className={`arrow ${activeIndex === 13 ? 'rotate' : ''}`} /></button>
+            <button className={`accordion ${activeIndex === 13 ? 'active' : ''}`} onClick={() => togglePanel(13)}>Are travel costs and lodging covered by TEDxUofW?<div className="arrowContainer"><img src={downArrow} alt="down arrow img" className={`arrow ${activeIndex === 13 ? 'rotate' : ''}`} /></div></button>
             <div className="panel" style={{ display: activeIndex === 13 ? 'block' : 'none' }}>
               <div className="panel-text">
                 <p>No, TEDxUofW does not cover travel costs or lodging for speakers.</p>
@@ -192,7 +192,7 @@ const Faq = () => {
         <h2>Sponsor FAQ</h2>
         <div className="dropdown-join-container">
           <div className="dropdown-ctr">
-            <button className={`accordion ${activeIndex === 14 ? 'active' : ''}`} onClick={() => togglePanel(14)}>I’d like to provide support or sponsor the event.<img src={downArrow} alt="down arrow img" className={`arrow ${activeIndex === 14 ? 'rotate' : ''}`} /></button>
+            <button className={`accordion ${activeIndex === 14 ? 'active' : ''}`} onClick={() => togglePanel(14)}>I’d like to provide support or sponsor the event.<div className="arrowContainer"><img src={downArrow} alt="down arrow img" className={`arrow ${activeIndex === 14 ? 'rotate' : ''}`} /></div></button>
             <div className="panel" style={{ display: activeIndex === 14 ? 'block' : 'none' }}>
               <div className="panel-text">
                 <p>Simply email <a href="mailto:tedxuofw@uw.edu">tedxuofw@uw.edu</a> and we’ll be sure to get you the information you
@@ -200,7 +200,7 @@ const Faq = () => {
               </div>
             </div>
 
-            <button className={`accordion ${activeIndex === 15 ? 'active' : ''}`} onClick={() => togglePanel(15)}>My company wants to provide a food/item/service to the attendees of at a TEDxUofW conference.<img src={downArrow} alt="down arrow img" className={`arrow ${activeIndex === 15 ? 'rotate' : ''}`} /></button>
+            <button className={`accordion ${activeIndex === 15 ? 'active' : ''}`} onClick={() => togglePanel(15)}>My company wants to provide a food/item/service to the attendees of at a TEDxUofW conference.<div className="arrowContainer"><img src={downArrow} alt="down arrow img" className={`arrow ${activeIndex === 15 ? 'rotate' : ''}`} /></div></button>
             <div className="panel" style={{ display: activeIndex === 15 ? 'block' : 'none' }}>
               <div className="panel-text">
                 <p>Thanks for your interest! Please contact <a href="mailto:tedxuofw@uw.edu">tedxuofw@uw.edu</a>, and we’ll be sure to get
@@ -208,7 +208,7 @@ const Faq = () => {
               </div>
             </div>
 
-            <button className={`accordion ${activeIndex === 16 ? 'active' : ''}`} onClick={() => togglePanel(16)}>What are the sponsorship benefits and tiers?<img src={downArrow} alt="down arrow img" className={`arrow ${activeIndex === 16 ? 'rotate' : ''}`} /></button>
+            <button className={`accordion ${activeIndex === 16 ? 'active' : ''}`} onClick={() => togglePanel(16)}>What are the sponsorship benefits and tiers?<div className="arrowContainer"><img src={downArrow} alt="down arrow img" className={`arrow ${activeIndex === 16 ? 'rotate' : ''}`} /></div></button>
             <div className="panel" style={{ display: activeIndex === 16 ? 'block' : 'none' }}>
               <div className="panel-text">
                 <p>We offer four tiers: Friend, Thinker, Educator, and Innovator, determined by the
@@ -219,7 +219,7 @@ const Faq = () => {
               </div>
             </div>
 
-            <button className={`accordion ${activeIndex === 17 ? 'active' : ''}`} onClick={() => togglePanel(17)}>What payment methods are accepted?<img src={downArrow} alt="down arrow img" className={`arrow ${activeIndex === 17 ? 'rotate' : ''}`} /></button>
+            <button className={`accordion ${activeIndex === 17 ? 'active' : ''}`} onClick={() => togglePanel(17)}>What payment methods are accepted?<div className="arrowContainer"><img src={downArrow} alt="down arrow img" className={`arrow ${activeIndex === 17 ? 'rotate' : ''}`} /></div></button>
             <div className="panel" style={{ display: activeIndex === 17 ? 'block' : 'none' }}>
               <div className="panel-text">
                 <p>For TEDxUofW, the preferred form of payment is by check made out to “TEDxUofW”.
