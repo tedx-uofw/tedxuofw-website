76c4af89e30a1124ca4f47ba8c76758fc0f28739
tashfia <tashfia@cs.washington.edu>
2024-05-14 19:16:45 -0700

    event-component

---
+++ src/components/event-component/event.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import "./event.css";
+import icon from '../../assets/upcomingIcon.svg';
+
+/**
+   * Creates event component which includes a picture of the event, event details, title, and a description
+   * @param {String} title title of event ie 2025 conference
+   * @param {String} name specific name of event ie do you have a minute
+   * @param {String} details event specific details ie date, time, location
+   * @param {String} description description of event
+   * @param {String} image path to event related image
+   */
+const Event = ({title, name, details, description, image}) => {
+
+  return (
+    <div id="event-card">
+      <img src={image} alt='event1'/>
+        <section id='event-info'>
+          <section id='event-header'>
+            <img src={icon} alt='icon'/>
+            <p>{title}</p>
+          </section>
+          <h1 id='event-name'>{name}</h1>
+          <h2 id='event-details'>{details}</h2>
+          <hr id='event-hr' width="100%" size="4"></hr>
+          <p id='event-description'>{description}</p>
+          <button id='event-website'>VIEW WEBSITE</button>
+        </section>
+    </div>
+  );
+};
+
+export default Event;
--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import './landing.css';
-import upcomingEvent1 from './landing-images/upcomingEvent1.png';
-import icon from './landing-images/upcomingIcon.svg';
+import upcomingEvent1 from '../assets/upcomingEvent1.png';
+import Event from './event-component/event.jsx';
 
 const landing = () => {
+
   return (
     <div id='landing'>
-      <div class='event-card'>
+      <Event
+        title='2025 CONFERENCE'
+        name='DO YOU HAVE A MINUTE?'
+        details='April 56, 1983 , 6AM-10PM | Kane Hall 301'
+        description='fun descriptor text about how amazing this conference is and oh
+          my gosh it’s going to be so great guys im so tired i just finished work and honestly i
+          just want to sleep but i have so much work to do rahhhhhhhh also the image will probably
+          hold the conference promo or branding somewhere'
+        image={upcomingEvent1}
+      />
+    </div>
+  )
+}
+
+export default landing
+
+
+/* <div class='event-card'>
         <img src={upcomingEvent1} alt='event1'/>
         <section id='event-info'>
           <section id='event-header'>
@@ -15,7 +33,7 @@ const landing = () => {
           </section>
           <h1 id='event-title'>DO YOU HAVE A MINUTE?</h1>
           <h2 id='event-details'>April 56, 1983 , 6AM-10PM | Kane Hall 301</h2>
-          <hr width="100%" size="2"></hr>
+          <hr id='event-hr' width="100%" size="4"></hr>
           <p id='event-description'>
             fun descriptor text about how amazing this conference is and oh
             my gosh it’s going to be so great guys im so tired i just finished work and honestly i
@@ -24,9 +42,4 @@ const landing = () => {
           </p>
           <button id='event-website'>VIEW WEBSITE</button>
         </section>
-      </div>
-    </div>
-  )
-}
-
-export default landing
+      </div> */
