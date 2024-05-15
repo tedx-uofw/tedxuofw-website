3c4388d2ec55bfa9dede47b1dad0ea97343d0d37
tedx-uofw <tedxuofw@uw.edu>
2024-05-15 02:23:34 -0700

    added styles to past events page

--- src/components/past-events/PastEvents.jsx
+++ src/components/past-events/PastEvents.jsx
@@ -31,27 +31,34 @@ const PastEvents = () => {
 
   return (
     <div className='past-events-container'>
-      <div className='past-events-title'>
-        <h1 className='events-heading'>PAST EVENTS</h1>
-        {items.length > 0 && (<Dropdown items={items} onSelect={onDropdownSelect} /> )}
-      </div>
+      <div className='events-ctr'>
+        <div className='past-events-title'>
+          <h1 className='events-heading'>PAST EVENTS</h1>
+          {items.length > 0 && (<Dropdown items={items} onSelect={onDropdownSelect} /> )}
+        </div>
 
-      <hr></hr>
+        <hr className='past-events-hr'></hr>
+      </div>
 
       <div className='events-container'>
         <h1 className='events-heading'>SPEAKERS</h1>
-        <hr></hr>
-        {selectedYear?.speakers?.map((speaker) => (
-          <CreateBox name={speaker.name} title={speaker.title} description={speaker.description} img={speaker.imgUrl} youtubeLink={speaker.youtubeUrl} linkedinLink={speaker.linkedinUrl}/>
-        ))}
+        <hr className='past-events-hr'></hr>
+        <div className='boxes-ctr'>
+          {selectedYear?.speakers?.map((speaker) => (
+            <CreateBox name={speaker.name} title={speaker.title} description={speaker.description} img={speaker.imgUrl} youtubeLink={speaker.youtubeUrl} linkedinLink={speaker.linkedinUrl}/>
+          ))}
+        </div>
+
       </div>
 
       <div className='events-container'>
         <h1 className='events-heading'>PERFORMERS</h1>
-        <hr></hr>
-        {selectedYear?.performers?.map((performer) => (
-          <CreateBox name={performer.name} title={performer.title} description={performer.description} img={performer.imgUrl} youtubeLink={performer.youtubeUrl} linkedinLink={performer.linkedinUrl}/>
-        ))}
+        <hr className='past-events-hr'></hr>
+        <div className='boxes-ctr'>
+          {selectedYear?.performers?.map((performer) => (
+            <CreateBox name={performer.name} title={performer.title} description={performer.description} img={performer.imgUrl} youtubeLink={performer.youtubeUrl} linkedinLink={performer.linkedinUrl}/>
+          ))}
+        </div>
       </div>
 
 
--- src/components/past-events/createBox.jsx
+++ src/components/past-events/createBox.jsx
@@ -12,20 +12,23 @@ const CreateBox = (props) => {
         <div className='box-content'>
             <img className='box-img' src={props.img} alt={props.name}/>
             <div className='title-content'>
-                <div className='name-box'>
+                <div className='name-socials-box'>
                     <h3 className='name-content'>{props.name}</h3>
-                    <p className='name-content'>{props.title}</p>
-                </div>
-                <div className='socials-box'>
-                    <a target='_blank' href={props.youtubeLink} className='social-link'>
-                        <img src={youtube}/>
-                    </a>
-                    <a target='_blank' href={props.linkedinLink} className='social-link'>
-                        <img src={linkedin}/>
-                    </a>
-                </div>
-            </div>
+                    <div className='socials-box'>
+                        <a target='_blank' href={props.youtubeLink} className='social-link'>
+                            <img src={youtube}/>
+                        </a>
+                        <a target='_blank' href={props.linkedinLink} className='social-link'>
+                            <img src={linkedin}/>
+                        </a>
+                    </div>
 
+                </div>
+                <p className='talk-title-content'>{props.title}</p>
+
+
+            </div>
+            <hr className='box-hr'></hr>
             <p className='box-description'>{props.description}</p>
         </div>
     </section>
