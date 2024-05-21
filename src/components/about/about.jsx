5b8ad5ee0b598aa808d700a197e877ed8fddf2c6
Salma <salmaaly47@gmail.com>
2024-05-20 17:29:47 -0700

    fixed about page

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -13,19 +13,15 @@ const about = () => {
     <div id="about">
       {/* TOP */}
       <h1 id="header">ABOUT</h1>
-      <div class="bar">
-        <div class="location">
-          <p>Founded: <span class="white">2012</span></p>
-          <p>Location: <span class="white">Seattle, WA</span></p>
-        </div>
-        <svg xmlns="http://www.w3.org/2000/svg" width="1470" height="2" viewBox="0 0 1470 2" fill="none">
-          <path d="M1470 1L-2.86102e-05 1" stroke="#A9A9A9"/>
-        </svg>        
+      <div class="location">
+        <p>Founded: <span class="white">2012</span></p>
+        <p>Location: <span class="white">Seattle, WA</span></p>
       </div>
 
+
       {/* TEDXUOFW */}
-      <div id="tedxuofw">
-        <div>
+      <div id="tedxuofw" class="bar">
+        <div class="about-col">
           <img src={tedxuofw_logo} alt="Logo of TEDxUofW" id="logo"/>
               <p>TEDxUofW Lorem ipsum dolor sit amet consectetur. Eget lorem pulvinar cras fermentum vulputate eget et. 
                 Eget tortor dictum amet morbi arcu adipiscing varius. Lacinia dis purus quam congue lacus ornare adipiscing amet. 
@@ -42,7 +38,7 @@ const about = () => {
               />
             </div>
         </div>
-        <div>
+        <div class="carousel about-col">
           <img src = {tedxuofw}/>
         </div>
       </div>
@@ -50,65 +46,59 @@ const about = () => {
       {/* TEDX, TED */}
       <div class="about-row">
         <div id="tedx" class="about-col">
-          <div class="bar">
             <div class="location">
               <p>Founded: <span class="white">2009</span></p>
               <p>Location: <span class="white">New York, NY</span></p>
-            </div>
-            <svg xmlns="http://www.w3.org/2000/svg" width="588" height="2" viewBox="0 0 588 2" fill="none">
-              <path d="M588 1L-1.14441e-05 1" stroke="#A9A9A9"/>
-            </svg> 
+            </div>          
+          <div class="bar">
+            <img src={tedx_logo} alt="Logo of TED" id="logo"></img>
+                <p>TEDxUofW Lorem ipsum dolor sit amet consectetur. Eget lorem pulvinar cras fermentum vulputate eget et. 
+                  Eget tortor dictum amet morbi arcu adipiscing varius. Lacinia dis purus quam congue lacus ornare adipiscing amet. 
+                  Pellentesque nunc etiam amet aliquet vulputate dui aliquet ullamcorper volutpat. Lorem ipsum dolor sit amet consectetur. 
+                  Eget lorem pulvinar cras fermentum vulputate eget et. Eget tortor dictum amet morbi arcu adipiscing varius.</p>
+                <Button 
+                  title = 'TEDX WEBSITE' 
+                  link = ''
+                />
           </div>
-          <img src={tedx_logo} alt="Logo of TED" id="logo"></img>
-              <p>TEDxUofW Lorem ipsum dolor sit amet consectetur. Eget lorem pulvinar cras fermentum vulputate eget et. 
-                Eget tortor dictum amet morbi arcu adipiscing varius. Lacinia dis purus quam congue lacus ornare adipiscing amet. 
-                Pellentesque nunc etiam amet aliquet vulputate dui aliquet ullamcorper volutpat. Lorem ipsum dolor sit amet consectetur. 
-                Eget lorem pulvinar cras fermentum vulputate eget et. Eget tortor dictum amet morbi arcu adipiscing varius.</p>
-              <Button 
-                title = 'TEDX WEBSITE' 
-                link = ''
-              />
+
         </div>
         <div id="ted" class="about-col">
-        <div class="bar">
-            <div class="location">
-              <p>Founded: <span class="white">1984</span></p>
-              <p>Location: <span class="white">New York, NY</span></p>
-            </div>
-            <svg xmlns="http://www.w3.org/2000/svg" width="588" height="2" viewBox="0 0 588 2" fill="none">
-              <path d="M588 1L-1.14441e-05 1" stroke="#A9A9A9"/>
-            </svg> 
+          <div class="location">
+            <p>Founded: <span class="white">1984</span></p>
+            <p>Location: <span class="white">New York, NY</span></p>
           </div>
-          <img src={ted_logo} alt="Logo of TED" id="logo"/>
-              <p>TEDxUofW Lorem ipsum dolor sit amet consectetur. Eget lorem pulvinar cras fermentum vulputate eget et. 
-                Eget tortor dictum amet morbi arcu adipiscing varius. Lacinia dis purus quam congue lacus ornare adipiscing amet. 
-                Pellentesque nunc etiam amet aliquet vulputate dui aliquet ullamcorper volutpat. Lorem ipsum dolor sit amet consectetur. 
-                Eget lorem pulvinar cras fermentum vulputate eget et. Eget tortor dictum amet morbi arcu adipiscing varius.</p>
-              <Button 
-                title = 'TED WEBSITE' 
-                link = ''
-              />
+          <div class="bar">
+            <img src={ted_logo} alt="Logo of TED" id="logo"/>
+                <p>TEDxUofW Lorem ipsum dolor sit amet consectetur. Eget lorem pulvinar cras fermentum vulputate eget et. 
+                  Eget tortor dictum amet morbi arcu adipiscing varius. Lacinia dis purus quam congue lacus ornare adipiscing amet. 
+                  Pellentesque nunc etiam amet aliquet vulputate dui aliquet ullamcorper volutpat. Lorem ipsum dolor sit amet consectetur. 
+                  Eget lorem pulvinar cras fermentum vulputate eget et. Eget tortor dictum amet morbi arcu adipiscing varius.</p>
+                <Button 
+                  title = 'TED WEBSITE' 
+                  link = ''
+                />
+          </div>
+
           </div>        
       </div>
 
       {/* RO */}
-      <div id="ro">
-        <div>
+      <div id="ro" class="about-row">
+        <div class="about-col">
             <h1>Ro Verdeja</h1>
             <div class="bar">
-              <svg xmlns="http://www.w3.org/2000/svg" width="588" height="2" viewBox="0 0 588 2" fill="none">
-                <path d="M588 1L-1.14441e-05 1" stroke="#A9A9A9"/>
-              </svg> 
-            </div>
               <p>Special thanks to Ro who has been with us from the beginning. Speaker coach, presentation designer, curator and slide police and constant support. 
                 Since 2018 Ro has donated his time and expertise to our conference, coaching more than 40 speakers and 6 teams - elevating the level of the conference 
                 and the value for the audience by helping each of us on the stage today do our best work.</p>
               <Button 
                 title = 'LINKEDIN' 
                 link = ''
-              />
+              />    
+            </div>
         </div>
-        <div>
+
+        <div class="about-col">
           <img src = {ro}/>
         </div>
       </div>
