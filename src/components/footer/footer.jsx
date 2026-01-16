e6737c04f11edc741c342d0a0e120a2ce07a415b
Shashvath-S <119080138+Shashvath-S@users.noreply.github.com>
2026-01-15 17:38:06 -0800

    Adding spacing for copyright line and added space between copyright symbol and text

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import './footer.css'
-import logo from '../../assets/logo.png'
+import React from "react";
+import "./footer.css";
+import logo from "../../assets/logo.png";
 import email from "../../assets/social-media/email.svg";
 import facebook from "../../assets/social-media/facebook.svg";
 import instagram from "../../assets/social-media/instagram.svg";
@@ -9,16 +9,23 @@ import tiktok from "../../assets/social-media/tiktok.svg";
 
 const footer = () => {
   return (
-<div id="footer">
-        <footer>
-          <div className='testing'>
+    <div id="footer">
+      <footer>
+        <div className="testing">
           {/* Top half of footer */}
           <div id="top">
             {/* Logo */}
-            <a href=""><img src={logo} alt="Logo of TEDxUofW" id="footer-logo" href=""></img></a>
+            <a href="">
+              <img
+                src={logo}
+                alt="Logo of TEDxUofW"
+                id="footer-logo"
+                href=""
+              ></img>
+            </a>
 
-             {/* Sitemap */}
-             <div id="pages">
+            {/* Sitemap */}
+            <div id="pages">
               <div className="section">
                 <div className="footer-row">
                   <div className="footer-col">
@@ -36,28 +43,43 @@ const footer = () => {
           {/* Second half of footer */}
           <div id="bottom">
             {/* Copyright */}
-            <p id="copyright">©TEDxUofW 2024 | This independent TEDx event is operated under license from TED.</p>
-            
-            <div className="mobile-only"><p>CONNECT WITH US:</p></div>
+            <p id="copyright">
+              © TEDxUofW 2024 | This independent TEDx event is operated under
+              license from TED.
+            </p>
+
+            <div className="mobile-only">
+              <p>CONNECT WITH US:</p>
+            </div>
 
             {/* Logos */}
-            <div id="icons">            
+            <div id="icons">
               {/* Mail */}
-              <a href="mailto:tedxuofw@uw.edu"><img src={email}></img></a>
+              <a href="mailto:tedxuofw@uw.edu">
+                <img src={email}></img>
+              </a>
               {/* Facebook */}
-              <a href="https://www.facebook.com/TEDxUofW"><img src={facebook}></img></a>
+              <a href="https://www.facebook.com/TEDxUofW">
+                <img src={facebook}></img>
+              </a>
               {/* Instagram */}
-              <a href="https://www.instagram.com/tedxuofw/"><img src={instagram}></img></a>
+              <a href="https://www.instagram.com/tedxuofw/">
+                <img src={instagram}></img>
+              </a>
               {/* TikTok */}
-              <a href="https://www.tiktok.com/@tedxuofw"><img src={tiktok}></img></a>
+              <a href="https://www.tiktok.com/@tedxuofw">
+                <img src={tiktok}></img>
+              </a>
               {/* LinkedIn */}
-              <a href="https://www.linkedin.com/company/tedxuofw/mycompany/"><img src={linkedin}></img></a>
+              <a href="https://www.linkedin.com/company/tedxuofw/mycompany/">
+                <img src={linkedin}></img>
+              </a>
             </div>
           </div>
-          </div>
-        </footer>
-      </div>
-  )
-}
+        </div>
+      </footer>
+    </div>
+  );
+};
 
-export default footer
+export default footer;
